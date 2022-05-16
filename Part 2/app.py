b43d48b59580fc59725acff5f5e13a5b2bfe76b5
GeFoGen <gefogen>
2022-05-16 21:49:38 +0300

    Part 2 completed

--- Part 2/app.py
+++ Part 2/app.py
@@ -1,10 +1,32 @@
-from flask import Flask  # Импортируем Flask
-import json  # Импортируем json
-
-with open("candidates.json", "r", encoding="utf-8") as file:
-    content: list = json.load(file)  # Импортируем данные из candidates.json в приложение
+from flask import Flask, request, render_template  # Импортируем Flask
+from utils import *     # Импортируем функции
 
 app = Flask(__name__)  # Создаем экземпляр Flask'а
 
+
+@app.route("/")     # Главная страница
+def load_main_page():
+    content = load_content()
+    return render_template("list_of_candidates.html", content=content, quantity=len(content))
+
+
+@app.route("/candidate/<int:id>")       # Поиск кандидата по id
+def load_candidate_by_id(id):
+    content = get_candidate(id)
+    return render_template("page_of_candidate.html", content=content)
+
+
+@app.route("/search/<name>")        # Поиск кандидата по имени
+def load_candidates_by_name(name):
+    content = get_candidates_by_name(name)
+    return render_template("search.html", content=content, quantity=len(content))
+
+
+@app.route("/skill/<skill_name>")       # Поиск кандидатов по навыкам
+def load_candidates_by_skills(skill_name):
+    content = get_candidates_by_skill(skill_name)
+    return render_template("skill.html", content=content, quantity=len(content))
+
+
 if __name__ == "__main__":
-    app.run(debug=True)     # Запускаем
+    app.run(debug=True)  # Запускаем
--- Part 2/utils.py
+++ Part 2/utils.py
@@ -1,2 +1,37 @@
-with open("candidates.json", "r", encoding="utf-8") as file:
-    content: list = json.load(file)  # Импортируем данные из candidates.json в приложение
+import json
+
+
+def load_content():
+    """ Импортируем данные из candidates.json """
+    with open("candidates.json", "r", encoding="utf-8") as file:
+        content: list = json.load(file)  # Импортируем данные из candidates.json в приложение
+        return content
+
+
+def get_candidate(id):
+    """ Возвращает данные кандидата """
+    content = load_content()  # Загружаем контент из json
+    for people in content:  # Перебор по списку
+        if people['id'] == id:  # Проверяем наличие кандидата
+            return people
+
+
+def get_candidates_by_skill(skill):
+    """ Функция, которая возвращает кандидатов по навыкам """
+    content = load_content()  # Загружаем контент из json
+    men = []  # Список, который мы будем возвращать в качестве результат
+    for people in content:  # Перебор по списку
+        chel = people["skills"].lower().split(", ")  # Переменная для поиска точного совпадения, а не соответствий
+        if skill in chel:
+            men.append(people)
+    return men
+
+
+def get_candidates_by_name(candidate_name):
+    """ Функция, которая возвращает кандидатов по имени """
+    content = load_content()  # Загружаем контент из json
+    men = []  # Список, который мы будем возвращать в качестве результат
+    for people in content:  # Перебор по списку
+        if candidate_name.lower() in people["name"].lower():
+            men.append(people)
+    return men
